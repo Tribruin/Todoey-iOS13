505e1b1cb71ef3ad7de60298e5342f527602d6ef
Tribruin <39201800+Tribruin@users.noreply.github.com>
2020-01-07 11:25:29 -0600

    Updated Commit

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Rob Blount on 1/7/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
